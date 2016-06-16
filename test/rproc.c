3c18d4a8f80943934439089567795a95b264a0c8
Haakon Sporsheim <haakon.sporsheim@gmail.com>
2016-06-16 12:57:53 +0200

    proc: Fix test for windows.

--- test/rproc.c
+++ test/rproc.c
@@ -7,10 +7,10 @@ RTEST (rproc, get_exe, RTEST_FAST | RTEST_SYSTEM)
   rchar * exe;
 
   r_assert_cmpptr ((exe = r_proc_get_exe_path ()), !=, NULL);
-  r_assert (r_str_has_suffix (exe, TEST_BINARY));
+  r_assert (r_str_has_suffix (exe, TEST_BINARY R_EXE_SUFFIX));
   r_free (exe);
 
-  r_assert_cmpstr ((exe = r_proc_get_exe_name ()), ==, TEST_BINARY);
+  r_assert_cmpstr ((exe = r_proc_get_exe_name ()), ==, TEST_BINARY R_EXE_SUFFIX);
   r_free (exe);
 }
 RTEST_END;
