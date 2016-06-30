44c5cb8a4268be1c657c884ed82bf00508af2d33
Haakon Sporsheim <haakon.sporsheim@gmail.com>
2016-06-30 08:58:19 +0200

    str: Reorder funcion declaration.

--- rlib/rstr.h
+++ rlib/rstr.h
@@ -31,6 +31,7 @@ R_BEGIN_DECLS
 
 R_API rsize r_strlen (const rchar * str);
 
+/* Compare strings */
 R_API int r_strcmp (const rchar * a, const rchar * b);
 R_API int r_strncmp (const rchar * a, const rchar * b, rsize len);
 #define r_str_equals(a,b) (r_strcmp (a, b) == 0)
@@ -39,6 +40,7 @@ R_API rboolean r_str_has_prefix (const rchar * str, const rchar * prefix);
 R_API rboolean r_str_has_suffix (const rchar * str, const rchar * suffix);
 #define r_str_has_substring(str, sub) (r_str_idx_of_str (str, -1, sub, -1) >= 0)
 
+/* Search/scan for characters/strings */
 R_API rsize r_strspn (const rchar * str, const rchar * set);
 R_API rsize r_strcspn (const rchar * str, const rchar * cset);
 R_API rchar * r_strchr (const rchar * str, int c);
@@ -55,6 +57,7 @@ R_API rssize r_str_idx_of_c_any (const rchar * str, rssize strsize,
 R_API rssize r_str_idx_of_str (const rchar * str, rssize strsize,
     const rchar * sub, rssize subsize);
 
+/* Copy and concatenate */
 R_API rchar * r_strcpy (rchar * dst, const rchar * src);
 R_API rchar * r_strncpy (rchar * dst, const rchar * src, rsize len);
 R_API rchar * r_strcat (rchar * dst, const rchar * src);
@@ -62,6 +65,26 @@ R_API rchar * r_strncat (rchar * dst, const rchar * src, rsize len);
 R_API rchar * r_stpcpy (rchar * dst, const rchar * src);
 R_API rchar * r_stpncpy (rchar * dst, const rchar * src, rsize len);
 
+R_API rchar * r_strdup (const rchar * str);
+R_API rchar * r_strndup (const rchar * str, rsize n);
+R_API rchar * r_strdup_wstrip (const rchar * str);
+R_API rchar * r_strdup_strip (const rchar * str, const rchar * chars);
+
+/* Strip for whitespace or specific characters */
+R_API rchar * r_str_wstrip (rchar * str);
+R_API const rchar * r_str_lwstrip (const rchar * str);
+R_API rchar * r_str_twstrip (rchar * str);
+
+R_API rchar * r_str_strip (rchar * str, const rchar * chars);
+R_API const rchar * r_str_lstrip (const rchar * str, const rchar * chars);
+R_API rchar * r_str_tstrip (rchar * str, const rchar * chars);
+
+/* Join and split */
+R_API rchar * r_strjoin (const rchar * delim, ...);
+R_API rchar * r_strjoinv (const rchar * delim, va_list args);
+R_API rchar ** r_strsplit (const rchar * str, const rchar * delim, rsize max);
+
+/* Parsing from string to numbers (integers and floats) */
 typedef enum {
   R_STR_PARSE_OK,
   R_STR_PARSE_RANGE,
@@ -117,19 +140,7 @@ R_API rdouble r_str_to_double (const rchar * str, const rchar ** endptr, RStrPar
 #define r_strtof(str, endptr) r_str_to_float  (str, endptr, NULL)
 #define r_strtod(str, endptr) r_str_to_double (str, endptr, NULL)
 
-R_API rchar * r_strdup (const rchar * str);
-R_API rchar * r_strndup (const rchar * str, rsize n);
-R_API rchar * r_strdup_wstrip (const rchar * str);
-R_API rchar * r_strdup_strip (const rchar * str, const rchar * chars);
-
-R_API rchar * r_str_wstrip (rchar * str);
-R_API const rchar * r_str_lwstrip (const rchar * str);
-R_API rchar * r_str_twstrip (rchar * str);
-
-R_API rchar * r_str_strip (rchar * str, const rchar * chars);
-R_API const rchar * r_str_lstrip (const rchar * str, const rchar * chars);
-R_API rchar * r_str_tstrip (rchar * str, const rchar * chars);
-
+/* Formatting strings */
 #define r_sprintf       sprintf
 #define r_vsprintf      vsprintf
 #define r_snprintf      snprintf
@@ -139,9 +150,20 @@ R_API int     r_vasprintf (rchar ** str, const rchar * fmt, va_list args) R_ATTR
 R_API rchar * r_strprintf (const rchar * fmt, ...) R_ATTR_PRINTF (1, 2);
 R_API rchar * r_strvprintf (const rchar * fmt, va_list args) R_ATTR_PRINTF (1, 0);
 
+/* Dump/format memory to string */
+#define R_STR_MEM_DUMP_SIZE(align) \
+  ((align * 4) + (align / 4) + (RLIB_SIZEOF_VOID_P * 2) + 8)
+R_API rboolean r_str_mem_dump (rchar * str, const ruint8 * ptr,
+    rsize size, rsize align);
+R_API rchar * r_str_mem_dump_dup (const ruint8 * ptr,
+    rsize size, rsize align);
+R_API rchar * r_str_mem_hex (const ruint8 * ptr, rsize size);
+
+/* Scanning */
 R_API int     r_strscanf (const rchar * str, const rchar * fmt, ...) R_ATTR_SCANF (2, 3);
 R_API int     r_strvscanf (const rchar * str, const rchar * fmt, va_list args) R_ATTR_SCANF (2, 0);
 
+/* String list */
 R_API RSList * r_str_list_new (const rchar * str0, ...);
 R_API RSList * r_str_list_newv (const rchar * str0, va_list args);
 
@@ -153,18 +175,6 @@ R_API rsize r_strv_len (rchar * const * strv);
 R_API void r_strv_foreach (rchar ** strv, RFunc func, rpointer user);
 R_API rboolean r_strv_contains (rchar * const * strv, const rchar * str);
 R_API rchar * r_strv_join (rchar * const * strv, const rchar * delim);
-
-R_API rchar * r_strjoin (const rchar * delim, ...);
-R_API rchar * r_strjoinv (const rchar * delim, va_list args);
-R_API rchar ** r_strsplit (const rchar * str, const rchar * delim, rsize max);
-
-#define R_STR_MEM_DUMP_SIZE(align) \
-  ((align * 4) + (align / 4) + (RLIB_SIZEOF_VOID_P * 2) + 8)
-R_API rboolean r_str_mem_dump (rchar * str, const ruint8 * ptr,
-    rsize size, rsize align);
-R_API rchar * r_str_mem_dump_dup (const ruint8 * ptr,
-    rsize size, rsize align);
-R_API rchar * r_str_mem_hex (const ruint8 * ptr, rsize size);
 
 R_END_DECLS
 
