61ce2354e1260ca5f502027e0c2d0f5780028660
Haakon Sporsheim <haakon.sporsheim@gmail.com>
2016-09-08 13:26:47 +0200

    bench: Add system info.

--- bench/main.c
+++ bench/main.c
@@ -1,5 +1,16 @@
 #include <rlib/rlib.h>
 
+RTEST_BENCH (rbench, system, RTEST_FAST | RTEST_SYSTEM)
+{
+  r_print ("%"R_TIME_FORMAT" --- %s ---\n", R_TIME_ARGS (0), R_STRFUNC);
+  r_print ("\tCPU:\n");
+  r_print ("\t\tPackages: %4u\n", r_sys_cpu_packages ());
+  r_print ("\t\tMAX:      %4u\n", r_sys_cpu_max_count ());
+  r_print ("\t\tPyhsical: %4u\n", r_sys_cpu_physical_count ());
+  r_print ("\t\tLogical:  %4u\n", r_sys_cpu_logical_count ());
+}
+RTEST_END;
+
 /********************************************************/
 /* Main entry point and test runner                     */
 /********************************************************/
