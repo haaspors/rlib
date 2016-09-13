c66a631efef3481074694c1bf02ec21b85407b77
Haakon Sporsheim <haakon.sporsheim@gmail.com>
2016-09-13 08:41:38 +0200

    bench: Add aggregates to udp multi_loopback_receive

--- bench/revudp.c
+++ bench/revudp.c
@@ -49,11 +49,21 @@ static void
 udp_recv (rpointer user, RBuffer * buf, const RSocketAddress * addr, REvUDP * evudp)
 {
   REvUDPBenchCtx * ctx = user;
+  rsize size;
   (void) evudp;
   (void) addr;
 
+  size = r_buffer_get_size (buf);
   ctx->rx.packets++;
-  ctx->rx.bytes += r_buffer_get_size (buf);
+  ctx->rx.bytes += size;
+}
+
+static void
+aggregate_ctx_stats (REvUDPStats * dst, const REvUDPStats * add)
+{
+  dst->packets += add->packets;
+  dst->failed += add->failed;
+  dst->bytes += add->bytes;
 }
 
 static void
@@ -124,21 +134,24 @@ RTEST_BENCH (revudp, single_loopback_receive, RTEST_FASTSLOW | RTEST_SYSTEM)
 }
 RTEST_END;
 
+
+#define UDP_MULTI_SOCKETS     16
 static void
-stop_16 (rpointer data, REvLoop * loop)
+stop_multi (rpointer data, REvLoop * loop)
 {
   REvUDPBenchCtx * ctx = data;
   ruint i;
   (void) loop;
 
-  for (i = 0; i < 16; i++)
+  for (i = 0; i < UDP_MULTI_SOCKETS; i++)
     r_assert (r_ev_udp_recv_stop (ctx[i].evudp));
 }
 
-RTEST_BENCH (revudp, 16_loopback_receive, RTEST_FASTSLOW | RTEST_SYSTEM)
+RTEST_BENCH (revudp, multi_loopback_receive, RTEST_FASTSLOW | RTEST_SYSTEM)
 {
   REvLoop * loop;
-  REvUDPBenchCtx ctx[16];
+  REvUDPBenchCtx ctx[UDP_MULTI_SOCKETS];
+  REvUDPStats tx_aggr, rx_aggr;
   ruint i;
   RClockTime ts;
 
@@ -160,18 +173,32 @@ RTEST_BENCH (revudp, 16_loopback_receive, RTEST_FASTSLOW | RTEST_SYSTEM)
     r_assert_cmpptr ((ctx[i].tsnd = r_thread_new ("send udp packets", udp_send, &ctx[i])), !=, NULL);
   }
 
-  r_assert (r_ev_loop_add_callback_later (loop, NULL, TOTAL_LENGTH, stop_16, ctx, NULL));
+  r_assert (r_ev_loop_add_callback_later (loop, NULL, TOTAL_LENGTH, stop_multi, ctx, NULL));
   r_ev_loop_run (loop, R_EV_LOOP_RUN_LOOP);
 
+  r_memclear (&tx_aggr, sizeof (REvUDPStats));
+  r_memclear (&rx_aggr, sizeof (REvUDPStats));
   ts = r_time_get_ts_monotonic ();
   for (i = 0; i < R_N_ELEMENTS (ctx); i++) {
     print_udp_bench_ctx_stats (&ctx[i], ts);
+    aggregate_ctx_stats (&tx_aggr, &ctx[i].tx);
+    aggregate_ctx_stats (&rx_aggr, &ctx[i].rx);
     ctx[i].running = FALSE;
     r_thread_join (ctx[i].tsnd);
     r_thread_unref (ctx[i].tsnd);
     r_socket_address_unref (ctx[i].addr);
     r_ev_udp_unref (ctx[i].evudp);
   }
+
+  ts = r_time_get_ts_monotonic ();
+  r_print ("%"R_TIME_FORMAT" --------------------------------------------------------------\n"
+      "                 "
+      "  TX: %12"RSIZE_FMT" (%5"RSIZE_FMT") %16"RSIZE_FMT"\n"
+      "                 "
+      "  RX: %12"RSIZE_FMT" (%5"RSIZE_FMT") %16"RSIZE_FMT"\n",
+      R_TIME_ARGS (ts - ctx->start),
+      tx_aggr.packets, tx_aggr.failed, tx_aggr.bytes,
+      rx_aggr.packets, rx_aggr.failed, rx_aggr.bytes);
   r_ev_loop_unref (loop);
 }
 RTEST_END;
