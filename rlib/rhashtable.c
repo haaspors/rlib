033409f91d353208e58f73bdee999bacbdcc57a0
Haakon Sporsheim <haakon.sporsheim@gmail.com>
2017-01-04 10:19:36 +0100

    hashtable: Share macros with RHashSet.

--- rlib/rhashtable.c
+++ rlib/rhashtable.c
@@ -23,9 +23,6 @@
 
 #include <rlib/rmem.h>
 
-#define R_HASH_TABLE_EMPTY                          RSIZE_MAX
-#define R_HASH_TABLE_ALLOC_IDX_TO_SIZE(idx)         ((rsize)1 << (idx + 3))
-
 typedef struct {
   rpointer key;
   rpointer val;
@@ -37,7 +34,6 @@ struct _RHashTable {
 
   rsize size;
   ruint8 allocidx;
-
   RHashTableBucket * buckets;
 
   RHashFunc hashfunc;
@@ -45,6 +41,330 @@ struct _RHashTable {
   RDestroyNotify keynotify;
   RDestroyNotify valuenotify;
 };
+
+static void
+r_hash_table_free (RHashTable * ht)
+{
+  rsize i, c = R_HASH_CONTAINER_ALLOC_IDX_TO_SIZE (ht->allocidx);
+
+  if (ht->keynotify != NULL && ht->valuenotify != NULL) {
+    for (i = 0; i < c; i++) {
+      if (ht->buckets[i].hash != R_HASH_EMPTY) {
+        ht->keynotify (ht->buckets[i].key);
+        ht->valuenotify (ht->buckets[i].val);
+      }
+    }
+  } else if (ht->valuenotify != NULL) {
+    for (i = 0; i < c; i++) {
+      if (ht->buckets[i].hash != R_HASH_EMPTY)
+        ht->valuenotify (ht->buckets[i].val);
+    }
+  } else if (ht->keynotify != NULL) {
+    for (i = 0; i < c; i++) {
+      if (ht->buckets[i].hash != R_HASH_EMPTY)
+        ht->keynotify (ht->buckets[i].key);
+    }
+  }
+
+  r_free (ht->buckets);
+  r_free (ht);
+}
+
+static void
+r_hash_table_resize (RHashTable * ht, ruint8 allocidx)
+{
+  RHashTableBucket * buckets = ht->buckets;
+  rsize i, size;
+
+  size = R_HASH_CONTAINER_ALLOC_IDX_TO_SIZE (allocidx);
+  ht->buckets = r_mem_new_n (RHashTableBucket, size);
+  for (i = 0; i < size; i++)
+    ht->buckets[i].hash = R_HASH_EMPTY;
+
+  size = R_HASH_CONTAINER_ALLOC_IDX_TO_SIZE (ht->allocidx);
+  ht->allocidx = allocidx;
+
+  for (i = 0; i < size; i++) {
+    rsize idx, step = 0;
+
+    if (buckets[i].hash == R_HASH_EMPTY)
+      continue;
+
+    idx = buckets[i].hash % r_hash_size_primes[ht->allocidx];
+    while (ht->buckets[idx].hash != R_HASH_EMPTY) {
+      idx += ++step;
+      idx &= (R_HASH_CONTAINER_ALLOC_IDX_TO_SIZE (ht->allocidx) - 1);
+    }
+
+    ht->buckets[idx] = buckets[i];
+  }
+
+  r_free (buckets);
+}
+
+RHashTable *
+r_hash_table_new_full (RHashFunc hash, REqualFunc equal,
+    RDestroyNotify keynotify, RDestroyNotify valuenotify)
+{
+  RHashTable * ret;
+
+  if ((ret = r_mem_new (RHashTable)) != NULL) {
+    rsize i, size;
+    r_ref_init (ret, r_hash_table_free);
+
+    ret->size = 0;
+    ret->allocidx = 0;
+    size = R_HASH_CONTAINER_ALLOC_IDX_TO_SIZE (ret->allocidx);
+    ret->buckets = r_mem_new_n (RHashTableBucket, size);
+    for (i = 0; i < size; i++)
+      ret->buckets[i].hash = R_HASH_EMPTY;
+    ret->hashfunc = hash != NULL ? hash : r_direct_hash;
+    ret->equalfunc = equal;
+    ret->keynotify = keynotify;
+    ret->valuenotify = valuenotify;
+  }
+
+  return ret;
+}
+
+rsize
+r_hash_table_size (RHashTable * ht)
+{
+  return ht->size;
+}
+
+rsize
+r_hash_table_current_alloc_size (RHashTable * ht)
+{
+  return R_HASH_CONTAINER_ALLOC_IDX_TO_SIZE (ht->allocidx);
+}
+
+static inline rsize
+r_hash_table_hash (RHashTable * ht, rconstpointer key)
+{
+  rsize ret = ht->hashfunc (key);
+  if (R_UNLIKELY (ret == R_HASH_EMPTY))
+    ret++;
+  return ret;
+}
+
+static rsize
+r_hash_table_lookup_bucket (RHashTable * ht, rconstpointer key, rsize * hash)
+{
+  rsize idx, step = 0;
+
+  *hash = r_hash_table_hash (ht, key);
+  idx = *hash % r_hash_size_primes[ht->allocidx];
+
+  while (ht->buckets[idx].hash != R_HASH_EMPTY) {
+    if (ht->buckets[idx].hash == *hash) {
+      if ((ht->equalfunc == NULL && key == ht->buckets[idx].key) ||
+          ht->equalfunc (key, ht->buckets[idx].key))
+        break;
+    }
+
+    idx += ++step;
+    idx &= (R_HASH_CONTAINER_ALLOC_IDX_TO_SIZE (ht->allocidx) - 1);
+  }
+
+  return idx;
+}
+
+RHashTableError
+r_hash_table_insert (RHashTable * ht, rpointer key, rpointer value)
+{
+  rsize idx, hash;
+
+  if (R_UNLIKELY (ht == NULL)) return R_HASH_TABLE_INVAL;
+
+  if (ht->size >= R_HASH_CONTAINER_ALLOC_IDX_TO_SIZE (ht->allocidx))
+    r_hash_table_resize (ht, ht->allocidx + 1);
+
+  idx = r_hash_table_lookup_bucket (ht, key, &hash);
+  if (ht->buckets[idx].hash == hash) {
+    if (ht->keynotify != NULL)
+      ht->keynotify (ht->buckets[idx].key);
+    if (ht->valuenotify != NULL)
+      ht->valuenotify (ht->buckets[idx].val);
+  } else {
+    ht->buckets[idx].hash = hash;
+    ht->size++;
+  }
+
+  ht->buckets[idx].key = key;
+  ht->buckets[idx].val = value;
+  return R_HASH_TABLE_OK;
+}
+
+rpointer
+r_hash_table_lookup (RHashTable * ht, rconstpointer key)
+{
+  rsize idx, hash;
+  idx = r_hash_table_lookup_bucket (ht, key, &hash);
+  return (ht->buckets[idx].hash == hash) ? ht->buckets[idx].val : NULL;
+}
+
+RHashTableError
+r_hash_table_lookup_full (RHashTable * ht, rconstpointer key,
+    rpointer * keyout, rpointer * valueout)
+{
+  rsize idx, hash;
+
+  if (R_UNLIKELY (ht == NULL)) return R_HASH_TABLE_INVAL;
+
+  idx = r_hash_table_lookup_bucket (ht, key, &hash);
+  if (ht->buckets[idx].hash == hash) {
+    if (keyout != NULL)
+      *keyout = ht->buckets[idx].key;
+    if (valueout != NULL)
+      *valueout = ht->buckets[idx].val;
+    return R_HASH_TABLE_OK;
+  }
+
+  return R_HASH_TABLE_NOT_FOUND;
+}
+
+RHashTableError
+r_hash_table_contains (RHashTable * ht, rconstpointer key)
+{
+  rsize idx, hash;
+
+  if (R_UNLIKELY (ht == NULL)) return R_HASH_TABLE_INVAL;
+
+  idx = r_hash_table_lookup_bucket (ht, key, &hash);
+  return ht->buckets[idx].hash == hash ? R_HASH_TABLE_OK : R_HASH_TABLE_NOT_FOUND;
+}
+
+void
+r_hash_table_remove_all (RHashTable * ht)
+{
+  rsize i, c;
+
+  if (R_UNLIKELY (ht == NULL)) return;
+  if (R_UNLIKELY (ht->size == 0)) return;
+
+  c = R_HASH_CONTAINER_ALLOC_IDX_TO_SIZE (ht->allocidx);
+
+  if (ht->keynotify != NULL && ht->valuenotify != NULL) {
+    for (i = 0; i < c; i++) {
+      if (ht->buckets[i].hash == R_HASH_EMPTY) continue;
+      ht->keynotify (ht->buckets[i].key);
+      ht->valuenotify (ht->buckets[i].val);
+      ht->buckets[i].hash = R_HASH_EMPTY;
+    }
+  } else if (ht->valuenotify != NULL) {
+    for (i = 0; i < c; i++) {
+      if (ht->buckets[i].hash == R_HASH_EMPTY) continue;
+      ht->valuenotify (ht->buckets[i].val);
+      ht->buckets[i].hash = R_HASH_EMPTY;
+    }
+  } else if (ht->keynotify != NULL) {
+    for (i = 0; i < c; i++) {
+      if (ht->buckets[i].hash == R_HASH_EMPTY) continue;
+      ht->keynotify (ht->buckets[i].key);
+      ht->buckets[i].hash = R_HASH_EMPTY;
+    }
+  }
+
+  ht->size = 0;
+}
+
+RHashTableError
+r_hash_table_remove (RHashTable * ht, rconstpointer key)
+{
+  rsize idx, hash;
+
+  if (R_UNLIKELY (ht == NULL)) return R_HASH_TABLE_INVAL;
+
+  idx = r_hash_table_lookup_bucket (ht, key, &hash);
+  if (R_UNLIKELY (ht->buckets[idx].hash != hash))
+    return R_HASH_TABLE_NOT_FOUND;
+
+  if (ht->keynotify != NULL)
+    ht->keynotify (ht->buckets[idx].key);
+  if (ht->valuenotify != NULL)
+    ht->valuenotify (ht->buckets[idx].val);
+  ht->buckets[idx].hash = R_HASH_EMPTY;
+
+  ht->size--;
+  return R_HASH_TABLE_OK;
+}
+
+RHashTableError
+r_hash_table_remove_full (RHashTable * ht, rconstpointer key,
+    rpointer * keyout, rpointer * valueout)
+{
+  rsize idx, hash;
+
+  if (R_UNLIKELY (ht == NULL)) return R_HASH_TABLE_INVAL;
+
+  idx = r_hash_table_lookup_bucket (ht, key, &hash);
+  if (R_UNLIKELY (ht->buckets[idx].hash != hash)) {
+    if (keyout != NULL)
+      *keyout = NULL;
+    if (valueout != NULL)
+      *valueout = NULL;
+    return R_HASH_TABLE_NOT_FOUND;
+  }
+
+  if (keyout != NULL)
+    *keyout = ht->buckets[idx].key;
+  if (valueout != NULL)
+    *valueout = ht->buckets[idx].val;
+  if (ht->keynotify != NULL)
+    ht->keynotify (ht->buckets[idx].key);
+  if (ht->valuenotify != NULL)
+    ht->valuenotify (ht->buckets[idx].val);
+  ht->buckets[idx].hash = R_HASH_EMPTY;
+
+  ht->size--;
+  return R_HASH_TABLE_OK;
+}
+
+RHashTableError
+r_hash_table_steal (RHashTable * ht, rconstpointer key,
+    rpointer * keyout, rpointer * valueout)
+{
+  rsize idx, hash;
+
+  if (R_UNLIKELY (ht == NULL)) return R_HASH_TABLE_INVAL;
+
+  idx = r_hash_table_lookup_bucket (ht, key, &hash);
+  if (R_UNLIKELY (ht->buckets[idx].hash != hash)) {
+    if (keyout != NULL)
+      *keyout = NULL;
+    if (valueout != NULL)
+      *valueout = NULL;
+    return R_HASH_TABLE_NOT_FOUND;
+  }
+
+  if (keyout != NULL)
+    *keyout = ht->buckets[idx].key;
+  if (valueout != NULL)
+    *valueout = ht->buckets[idx].val;
+  ht->buckets[idx].hash = R_HASH_EMPTY;
+
+  ht->size--;
+  return R_HASH_TABLE_OK;
+}
+
+RHashTableError
+r_hash_table_foreach (RHashTable * ht, RKeyValueFunc func, rpointer user)
+{
+  rsize i, c;
+
+  if (R_UNLIKELY (ht == NULL)) return R_HASH_TABLE_INVAL;
+  if (R_UNLIKELY (func == NULL)) return R_HASH_TABLE_INVAL;
+
+  c = R_HASH_CONTAINER_ALLOC_IDX_TO_SIZE (ht->allocidx);
+  for (i = 0; i < c; i++) {
+    if (ht->buckets[i].hash != R_HASH_EMPTY)
+      func (ht->buckets[i].key, ht->buckets[i].val, user);
+  }
+
+  return R_HASH_TABLE_OK;
+}
+
 
 const rsize r_hash_size_primes[RLIB_SIZEOF_SIZE_T * 8 - 2] = {
                                         7,
@@ -114,327 +434,4 @@ const rsize r_hash_size_primes[RLIB_SIZEOF_SIZE_T * 8 - 2] = {
 #endif
 #endif
 };
-
-static void
-r_hash_table_free (RHashTable * ht)
-{
-  rsize i, c = R_HASH_TABLE_ALLOC_IDX_TO_SIZE (ht->allocidx);
-
-  if (ht->keynotify != NULL && ht->valuenotify != NULL) {
-    for (i = 0; i < c; i++) {
-      if (ht->buckets[i].hash != R_HASH_TABLE_EMPTY) {
-        ht->keynotify (ht->buckets[i].key);
-        ht->valuenotify (ht->buckets[i].val);
-      }
-    }
-  } else if (ht->valuenotify != NULL) {
-    for (i = 0; i < c; i++) {
-      if (ht->buckets[i].hash != R_HASH_TABLE_EMPTY)
-        ht->valuenotify (ht->buckets[i].val);
-    }
-  } else if (ht->keynotify != NULL) {
-    for (i = 0; i < c; i++) {
-      if (ht->buckets[i].hash != R_HASH_TABLE_EMPTY)
-        ht->keynotify (ht->buckets[i].key);
-    }
-  }
-
-  r_free (ht->buckets);
-  r_free (ht);
-}
-
-static void
-r_hash_table_resize (RHashTable * ht, ruint8 allocidx)
-{
-  RHashTableBucket * buckets = ht->buckets;
-  rsize i, size;
-
-  size = R_HASH_TABLE_ALLOC_IDX_TO_SIZE (allocidx);
-  ht->buckets = r_mem_new_n (RHashTableBucket, size);
-  for (i = 0; i < size; i++)
-    ht->buckets[i].hash = R_HASH_TABLE_EMPTY;
-
-  size = R_HASH_TABLE_ALLOC_IDX_TO_SIZE (ht->allocidx);
-  ht->allocidx = allocidx;
-
-  for (i = 0; i < size; i++) {
-    rsize idx, step = 0;
-
-    if (buckets[i].hash == R_HASH_TABLE_EMPTY)
-      continue;
-
-    idx = buckets[i].hash % r_hash_size_primes[ht->allocidx];
-    while (ht->buckets[idx].hash != R_HASH_TABLE_EMPTY) {
-      idx += ++step;
-      idx &= (R_HASH_TABLE_ALLOC_IDX_TO_SIZE (ht->allocidx) - 1);
-    }
-
-    ht->buckets[idx] = buckets[i];
-  }
-
-  r_free (buckets);
-}
-
-RHashTable *
-r_hash_table_new_full (RHashFunc hash, REqualFunc equal,
-    RDestroyNotify keynotify, RDestroyNotify valuenotify)
-{
-  RHashTable * ret;
-
-  if ((ret = r_mem_new (RHashTable)) != NULL) {
-    rsize i, size;
-    r_ref_init (ret, r_hash_table_free);
-
-    ret->size = 0;
-    ret->allocidx = 0;
-    size = R_HASH_TABLE_ALLOC_IDX_TO_SIZE (ret->allocidx);
-    ret->buckets = r_mem_new_n (RHashTableBucket, size);
-    for (i = 0; i < size; i++)
-      ret->buckets[i].hash = R_HASH_TABLE_EMPTY;
-    ret->hashfunc = hash != NULL ? hash : r_direct_hash;
-    ret->equalfunc = equal;
-    ret->keynotify = keynotify;
-    ret->valuenotify = valuenotify;
-  }
-
-  return ret;
-}
-
-rsize
-r_hash_table_size (RHashTable * ht)
-{
-  return ht->size;
-}
-
-rsize
-r_hash_table_current_alloc_size (RHashTable * ht)
-{
-  return R_HASH_TABLE_ALLOC_IDX_TO_SIZE (ht->allocidx);
-}
-
-static inline rsize
-r_hash_table_hash (RHashTable * ht, rconstpointer key)
-{
-  rsize ret = ht->hashfunc (key);
-  if (R_UNLIKELY (ret == R_HASH_TABLE_EMPTY))
-    ret++;
-  return ret;
-}
-
-static rsize
-r_hash_table_lookup_bucket (RHashTable * ht, rconstpointer key, rsize * hash)
-{
-  rsize idx, step = 0;
-
-  *hash = r_hash_table_hash (ht, key);
-  idx = *hash % r_hash_size_primes[ht->allocidx];
-
-  while (ht->buckets[idx].hash != R_HASH_TABLE_EMPTY) {
-    if (ht->buckets[idx].hash == *hash) {
-      if ((ht->equalfunc == NULL && key == ht->buckets[idx].key) ||
-          ht->equalfunc (key, ht->buckets[idx].key))
-        break;
-    }
-
-    idx += ++step;
-    idx &= (R_HASH_TABLE_ALLOC_IDX_TO_SIZE (ht->allocidx) - 1);
-  }
-
-  return idx;
-}
-
-RHashTableError
-r_hash_table_insert (RHashTable * ht, rpointer key, rpointer value)
-{
-  rsize idx, hash;
-
-  if (R_UNLIKELY (ht == NULL)) return R_HASH_TABLE_INVAL;
-
-  if (ht->size >= R_HASH_TABLE_ALLOC_IDX_TO_SIZE (ht->allocidx))
-    r_hash_table_resize (ht, ht->allocidx + 1);
-
-  idx = r_hash_table_lookup_bucket (ht, key, &hash);
-  if (ht->buckets[idx].hash == hash) {
-    if (ht->keynotify != NULL)
-      ht->keynotify (ht->buckets[idx].key);
-    if (ht->valuenotify != NULL)
-      ht->valuenotify (ht->buckets[idx].val);
-  } else {
-    ht->buckets[idx].hash = hash;
-    ht->size++;
-  }
-
-  ht->buckets[idx].key = key;
-  ht->buckets[idx].val = value;
-  return R_HASH_TABLE_OK;
-}
-
-rpointer
-r_hash_table_lookup (RHashTable * ht, rconstpointer key)
-{
-  rsize idx, hash;
-  idx = r_hash_table_lookup_bucket (ht, key, &hash);
-  return (ht->buckets[idx].hash == hash) ? ht->buckets[idx].val : NULL;
-}
-
-RHashTableError
-r_hash_table_lookup_full (RHashTable * ht, rconstpointer key,
-    rpointer * keyout, rpointer * valueout)
-{
-  rsize idx, hash;
-
-  if (R_UNLIKELY (ht == NULL)) return R_HASH_TABLE_INVAL;
-
-  idx = r_hash_table_lookup_bucket (ht, key, &hash);
-  if (ht->buckets[idx].hash == hash) {
-    if (keyout != NULL)
-      *keyout = ht->buckets[idx].key;
-    if (valueout != NULL)
-      *valueout = ht->buckets[idx].val;
-    return R_HASH_TABLE_OK;
-  }
-
-  return R_HASH_TABLE_NOT_FOUND;
-}
-
-RHashTableError
-r_hash_table_contains (RHashTable * ht, rconstpointer key)
-{
-  rsize idx, hash;
-
-  if (R_UNLIKELY (ht == NULL)) return R_HASH_TABLE_INVAL;
-
-  idx = r_hash_table_lookup_bucket (ht, key, &hash);
-  return ht->buckets[idx].hash == hash ? R_HASH_TABLE_OK : R_HASH_TABLE_NOT_FOUND;
-}
-
-void
-r_hash_table_remove_all (RHashTable * ht)
-{
-  rsize i, c;
-
-  if (R_UNLIKELY (ht == NULL)) return;
-  if (R_UNLIKELY (ht->size == 0)) return;
-
-  c = R_HASH_TABLE_ALLOC_IDX_TO_SIZE (ht->allocidx);
-
-  if (ht->keynotify != NULL && ht->valuenotify != NULL) {
-    for (i = 0; i < c; i++) {
-      if (ht->buckets[i].hash == R_HASH_TABLE_EMPTY) continue;
-      ht->keynotify (ht->buckets[i].key);
-      ht->valuenotify (ht->buckets[i].val);
-      ht->buckets[i].hash = R_HASH_TABLE_EMPTY;
-    }
-  } else if (ht->valuenotify != NULL) {
-    for (i = 0; i < c; i++) {
-      if (ht->buckets[i].hash == R_HASH_TABLE_EMPTY) continue;
-      ht->valuenotify (ht->buckets[i].val);
-      ht->buckets[i].hash = R_HASH_TABLE_EMPTY;
-    }
-  } else if (ht->keynotify != NULL) {
-    for (i = 0; i < c; i++) {
-      if (ht->buckets[i].hash == R_HASH_TABLE_EMPTY) continue;
-      ht->keynotify (ht->buckets[i].key);
-      ht->buckets[i].hash = R_HASH_TABLE_EMPTY;
-    }
-  }
-
-  ht->size = 0;
-}
-
-RHashTableError
-r_hash_table_remove (RHashTable * ht, rconstpointer key)
-{
-  rsize idx, hash;
-
-  if (R_UNLIKELY (ht == NULL)) return R_HASH_TABLE_INVAL;
-
-  idx = r_hash_table_lookup_bucket (ht, key, &hash);
-  if (R_UNLIKELY (ht->buckets[idx].hash != hash))
-    return R_HASH_TABLE_NOT_FOUND;
-
-  if (ht->keynotify != NULL)
-    ht->keynotify (ht->buckets[idx].key);
-  if (ht->valuenotify != NULL)
-    ht->valuenotify (ht->buckets[idx].val);
-  ht->buckets[idx].hash = R_HASH_TABLE_EMPTY;
-
-  ht->size--;
-  return R_HASH_TABLE_OK;
-}
-
-RHashTableError
-r_hash_table_remove_full (RHashTable * ht, rconstpointer key,
-    rpointer * keyout, rpointer * valueout)
-{
-  rsize idx, hash;
-
-  if (R_UNLIKELY (ht == NULL)) return R_HASH_TABLE_INVAL;
-
-  idx = r_hash_table_lookup_bucket (ht, key, &hash);
-  if (R_UNLIKELY (ht->buckets[idx].hash != hash)) {
-    if (keyout != NULL)
-      *keyout = NULL;
-    if (valueout != NULL)
-      *valueout = NULL;
-    return R_HASH_TABLE_NOT_FOUND;
-  }
-
-  if (keyout != NULL)
-    *keyout = ht->buckets[idx].key;
-  if (valueout != NULL)
-    *valueout = ht->buckets[idx].val;
-  if (ht->keynotify != NULL)
-    ht->keynotify (ht->buckets[idx].key);
-  if (ht->valuenotify != NULL)
-    ht->valuenotify (ht->buckets[idx].val);
-  ht->buckets[idx].hash = R_HASH_TABLE_EMPTY;
-
-  ht->size--;
-  return R_HASH_TABLE_OK;
-}
-
-RHashTableError
-r_hash_table_steal (RHashTable * ht, rconstpointer key,
-    rpointer * keyout, rpointer * valueout)
-{
-  rsize idx, hash;
-
-  if (R_UNLIKELY (ht == NULL)) return R_HASH_TABLE_INVAL;
-
-  idx = r_hash_table_lookup_bucket (ht, key, &hash);
-  if (R_UNLIKELY (ht->buckets[idx].hash != hash)) {
-    if (keyout != NULL)
-      *keyout = NULL;
-    if (valueout != NULL)
-      *valueout = NULL;
-    return R_HASH_TABLE_NOT_FOUND;
-  }
-
-  if (keyout != NULL)
-    *keyout = ht->buckets[idx].key;
-  if (valueout != NULL)
-    *valueout = ht->buckets[idx].val;
-  ht->buckets[idx].hash = R_HASH_TABLE_EMPTY;
-
-  ht->size--;
-  return R_HASH_TABLE_OK;
-}
-
-RHashTableError
-r_hash_table_foreach (RHashTable * ht, RKeyValueFunc func, rpointer user)
-{
-  rsize i, c;
-
-  if (R_UNLIKELY (ht == NULL)) return R_HASH_TABLE_INVAL;
-  if (R_UNLIKELY (func == NULL)) return R_HASH_TABLE_INVAL;
-
-  c = R_HASH_TABLE_ALLOC_IDX_TO_SIZE (ht->allocidx);
-  for (i = 0; i < c; i++) {
-    if (ht->buckets[i].hash != R_HASH_TABLE_EMPTY)
-      func (ht->buckets[i].key, ht->buckets[i].val, user);
-  }
-
-  return R_HASH_TABLE_OK;
-}
 
