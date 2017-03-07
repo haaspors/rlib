86615c328871aaa69e60b0cac0428f59f8e3243b
Haakon Sporsheim <haakon.sporsheim@gmail.com>
2017-03-07 08:45:19 +0100

    assert: Prefix internal macros with _.

--- rlib/rassert-internal.h
+++ rlib/rassert-internal.h
@@ -35,66 +35,66 @@
 #endif
 
 /* Assert helper macros */
-#define R_ASSERT_VA(...) r_test_assertion (R_LOG_CAT_ASSERT, __FILE__, __LINE__, R_STRFUNC, __VA_ARGS__)
+#define _R_ASSERT_VA(...) r_test_assertion (R_LOG_CAT_ASSERT, __FILE__, __LINE__, R_STRFUNC, __VA_ARGS__)
 #if defined(RLIB_COMPILATION)
-#define R_ASSERT_STMT(CMPEXPR, REPR, REAL, ...)                               \
+#define _R_ASSERT_STMT(CMPEXPR, REPR, REAL, ...)                              \
   __R_GCC_DISABLE_WARN_ADDRESS_BEGIN                                          \
   if (!(CMPEXPR)) abort ();                                                   \
   __R_GCC_DISABLE_WARN_ADDRESS_END
-#define R_ASSERT_STMT_CMPMEM(m1, cmp, m2, s, REPR, REAL, ...)                 \
+#define _R_ASSERT_STMT_CMPMEM(m1, cmp, m2, s, REPR, REAL, ...)                \
   __R_GCC_DISABLE_WARN_ADDRESS_BEGIN                                          \
-  if (!(__m1 && __m2 && r_memcmp (__m1, __m2, s) cmp 0)) abort ();              \
+  if (!(__m1 && __m2 && r_memcmp (__m1, __m2, s) cmp 0)) abort ();            \
   __R_GCC_DISABLE_WARN_ADDRESS_END
 #else
-#define R_ASSERT_STMT(CMPEXPR, REPR, REAL, ...)                               \
+#define _R_ASSERT_STMT(CMPEXPR, REPR, REAL, ...)                              \
   __R_GCC_DISABLE_WARN_ADDRESS_BEGIN                                          \
   if (CMPEXPR) R_LOG_CAT_TRACE (R_LOG_CAT_ASSERT, "%s ("REPR"): ("REAL")", "passed", __VA_ARGS__); \
-  else R_ASSERT_VA ("%s ("REPR"): ("REAL")", "*** assertion", __VA_ARGS__);   \
+  else _R_ASSERT_VA ("%s ("REPR"): ("REAL")", "*** assertion", __VA_ARGS__);  \
   __R_GCC_DISABLE_WARN_ADDRESS_END
-#define R_ASSERT_STMT_CMPMEM(m1, cmp, m2, size, REPR, REAL, ...)              \
+#define _R_ASSERT_STMT_CMPMEM(m1, cmp, m2, size, REPR, REAL, ...)             \
   __R_GCC_DISABLE_WARN_ADDRESS_BEGIN                                          \
-  if (m1 && m2 && r_memcmp (m1, m2, size) cmp 0) {                              \
+  if (m1 && m2 && r_memcmp (m1, m2, size) cmp 0) {                            \
     R_LOG_CAT_TRACE (R_LOG_CAT_ASSERT, "%s ("REPR"): ("REAL")", "passed", __VA_ARGS__);\
   } else {                                                                    \
     if (m1) r_log_mem_dump (R_LOG_CAT_ASSERT, R_LOG_LEVEL_ERROR,              \
         __FILE__, __LINE__, R_STRFUNC, m1, size, 16);                         \
     if (m2) r_log_mem_dump (R_LOG_CAT_ASSERT, R_LOG_LEVEL_ERROR,              \
         __FILE__, __LINE__, R_STRFUNC, m2, size, 16);                         \
-    R_ASSERT_VA ("%s ("REPR"): ("REAL")", "*** mem assertion", __VA_ARGS__);  \
+    _R_ASSERT_VA ("%s ("REPR"): ("REAL")", "*** mem assertion", __VA_ARGS__); \
   }                                                                           \
   __R_GCC_DISABLE_WARN_ADDRESS_END
 #endif
-#define R_ASSERT(expr) R_STMT_START {                                         \
+#define _R_ASSERT(expr) R_STMT_START {                                        \
   rboolean __res = (rboolean)(expr);                                          \
-  R_ASSERT_STMT (__res, "%s", "%u", #expr, __res)                             \
+  _R_ASSERT_STMT (__res, "%s", "%u", #expr, __res)                            \
 } R_STMT_END
-#define R_ASSERT_CMP(a1, cmp, a2, TYPE, FMT, r1, r2) R_STMT_START {           \
+#define _R_ASSERT_CMP(a1, cmp, a2, TYPE, FMT, r1, r2) R_STMT_START {          \
   TYPE __a1 = (TYPE)(a1), __a2 = (TYPE)(a2);                                  \
-  R_ASSERT_STMT (__a1 cmp __a2, "%s %s %s", FMT" %s "FMT,                     \
+  _R_ASSERT_STMT (__a1 cmp __a2, "%s %s %s", FMT" %s "FMT,                    \
       r1, #cmp, r2, __a1, #cmp, __a2);                                        \
 } R_STMT_END
-#define R_ASSERT_CMPSTR(s1, cmp, s2, r1, r2)   R_STMT_START {                 \
+#define _R_ASSERT_CMPSTR(s1, cmp, s2, r1, r2)   R_STMT_START {                \
   const rchar * __s1 = (s1), * __s2 = (s2);                                   \
-  R_ASSERT_STMT (r_strcmp (__s1, __s2) cmp 0, "%s %s %s", "\"%s\" %s \"%s\"", \
+  _R_ASSERT_STMT (r_strcmp (__s1, __s2) cmp 0, "%s %s %s", "\"%s\" %s \"%s\"",\
       r1, #cmp, r2, __s1, #cmp, __s2);                                        \
 } R_STMT_END
-#define R_ASSERT_CMPMEM(m1, cmp, m2, s, r1, r2)   R_STMT_START {              \
+#define _R_ASSERT_CMPMEM(m1, cmp, m2, s, r1, r2)   R_STMT_START {             \
   const ruint8 * __m1 = (const ruint8 *)(m1), * __m2 = (const ruint8 *)(m2);  \
-  R_ASSERT_STMT_CMPMEM (__m1, cmp, __m2, s,                                   \
+  _R_ASSERT_STMT_CMPMEM (__m1, cmp, __m2, s,                                  \
       "%s %s %s", "\"%p\" %s \"%p\"", r1, #cmp, r2, __m1, #cmp, __m2);        \
 } R_STMT_END
-#define R_ASSERT_LOG(expr, CAT, LVL, MSG) R_STMT_START {                      \
+#define _R_ASSERT_LOG(expr, CAT, LVL, MSG) R_STMT_START {                     \
   __R_GCC_DISABLE_WARN_ADDRESS_BEGIN                                          \
   RLogKeepLastCtx ctx;                                                        \
   r_log_keep_last_begin (&ctx, CAT);                                          \
   { expr; };                                                                  \
   r_log_keep_last_end (&ctx, FALSE, FALSE); /* Call _reset() later! */        \
   if (CAT != NULL)                                                            \
-    R_ASSERT_CMP (CAT,==,ctx.last.cat,rpointer,"%p","cat","<last log cat>");  \
+    _R_ASSERT_CMP (CAT,==,ctx.last.cat,rpointer,"%p","cat","<last log cat>"); \
   if (LVL != R_LOG_LEVEL_NONE)                                                \
-    R_ASSERT_CMP (LVL,==,ctx.last.lvl,ruint,"%u","lvl","<last log level>");   \
+    _R_ASSERT_CMP (LVL,==,ctx.last.lvl,ruint,"%u","lvl","<last log level>");  \
   if (MSG != NULL)                                                            \
-    R_ASSERT_CMPSTR (MSG,==,ctx.last.msg,"msg","<last log msg>");             \
+    _R_ASSERT_CMPSTR (MSG,==,ctx.last.msg,"msg","<last log msg>");            \
   r_log_keep_last_reset (&ctx);                                               \
   __R_GCC_DISABLE_WARN_ADDRESS_END                                            \
 } R_STMT_END
--- rlib/rassert.h
+++ rlib/rassert.h
@@ -1,5 +1,5 @@
 /* RLIB - Convenience library for useful things
- * Copyright (C) 2015  Haakon Sporsheim <haakon.sporsheim@gmail.com>
+ * Copyright (C) 2015-2017 Haakon Sporsheim <haakon.sporsheim@gmail.com>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -29,22 +29,22 @@
 
 R_BEGIN_DECLS
 
-#define r_assert(expr)                  R_ASSERT (expr)
-#define r_assert_not_reached()          R_ASSERT_VA ("<-- should not be reached")
-#define r_assert_cmpint(n1, cmp, n2)    R_ASSERT_CMP (n1,cmp,n2,rintmax,"%"RINTMAX_FMT,#n1,#n2)
-#define r_assert_cmpuint(n1, cmp, n2)   R_ASSERT_CMP (n1,cmp,n2,ruintmax,"%"RUINTMAX_FMT,#n1,#n2)
-#define r_assert_cmphex(n1, cmp, n2)    R_ASSERT_CMP (n1,cmp,n2,ruintmax,"%"RINTMAX_MODIFIER"x",#n1,#n2)
-#define r_assert_cmpptr(n1, cmp, n2)    R_ASSERT_CMP (n1,cmp,n2,rpointer,"%p",#n1,#n2)
-#define r_assert_cmpfloat(n1, cmp, n2)  R_ASSERT_CMP (n1,cmp,n2,rfloat,"%.9f",#n1,#n2)
-#define r_assert_cmpdouble(n1, cmp, n2) R_ASSERT_CMP (n1,cmp,n2,rdouble,"%.9f",#n1,#n2)
-#define r_assert_cmpstr(s1, cmp, s2)    R_ASSERT_CMPSTR (s1, cmp, s2, #s1, #s2)
-#define r_assert_cmpmem(m1, cmp, m2, s) R_ASSERT_CMPMEM (m1, cmp, m2, s, #m1, #m2)
+#define r_assert(expr)                          _R_ASSERT (expr)
+#define r_assert_not_reached()                  _R_ASSERT_VA ("<-- should not be reached")
+#define r_assert_cmpint(n1, cmp, n2)            _R_ASSERT_CMP (n1,cmp,n2,rintmax,"%"RINTMAX_FMT,#n1,#n2)
+#define r_assert_cmpuint(n1, cmp, n2)           _R_ASSERT_CMP (n1,cmp,n2,ruintmax,"%"RUINTMAX_FMT,#n1,#n2)
+#define r_assert_cmphex(n1, cmp, n2)            _R_ASSERT_CMP (n1,cmp,n2,ruintmax,"%"RINTMAX_MODIFIER"x",#n1,#n2)
+#define r_assert_cmpptr(n1, cmp, n2)            _R_ASSERT_CMP (n1,cmp,n2,rpointer,"%p",#n1,#n2)
+#define r_assert_cmpfloat(n1, cmp, n2)          _R_ASSERT_CMP (n1,cmp,n2,rfloat,"%.9f",#n1,#n2)
+#define r_assert_cmpdouble(n1, cmp, n2)         _R_ASSERT_CMP (n1,cmp,n2,rdouble,"%.9f",#n1,#n2)
+#define r_assert_cmpstr(s1, cmp, s2)            _R_ASSERT_CMPSTR (s1, cmp, s2, #s1, #s2)
+#define r_assert_cmpmem(m1, cmp, m2, s)         _R_ASSERT_CMPMEM (m1, cmp, m2, s, #m1, #m2)
 
 /* NOTE that r_assert_log* uses the RLogKeepLast framework which could affect performance */
-#define r_assert_logs_cat(expr, cat)          R_ASSERT_LOG (expr,  cat, 0, NULL)
-#define r_assert_logs_level(expr, l)          R_ASSERT_LOG (expr, NULL, l, NULL)
-#define r_assert_logs_msg(expr, msg)          R_ASSERT_LOG (expr, NULL, 0,  msg)
-#define r_assert_logs_full(expr, cat, l, msg) R_ASSERT_LOG (expr,  cat, l,  msg)
+#define r_assert_logs_cat(expr, cat)            _R_ASSERT_LOG (expr,  cat, 0, NULL)
+#define r_assert_logs_level(expr, l)            _R_ASSERT_LOG (expr, NULL, l, NULL)
+#define r_assert_logs_msg(expr, msg)            _R_ASSERT_LOG (expr, NULL, 0,  msg)
+#define r_assert_logs_full(expr, cat, l, msg)   _R_ASSERT_LOG (expr,  cat, l,  msg)
 
 
 R_API R_ATTR_NORETURN void r_test_assertion (RLogCategory * cat,
